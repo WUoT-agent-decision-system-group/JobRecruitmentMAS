cd07c7f6dbabf6ef3358d6dfdb54c9174b2ff0c2
nowakmikolaj <nowak.mikolaj19@gmail.com>
2024-11-28 21:57:46 +0100

    Reading agents configuration

--- app/agents/BaseAgent.py
+++ app/agents/BaseAgent.py
@@ -2,10 +2,19 @@ from abc import ABC, abstractmethod
 from time import sleep
 
 from spade.agent import Agent
+from utils import configuration as cfg
 
 
 class BaseAgent(ABC, Agent):
+    def __init__(self):
+        config = cfg.MASConfiguration.load()
+        agent_config = config.agents[self.__class__.__name__]
+        jid = agent_config.jid + "@" + config.server.name
+        super().__init__(jid, agent_config.password)
+
     async def start(self, auto_register: bool = True) -> None:
+        """Waits for server to initialize"""
+
         repeat = True
         while repeat is True:
             try:
@@ -15,10 +24,6 @@ class BaseAgent(ABC, Agent):
                 print('Server not found, trying again...')
                 sleep(3)
                 repeat = True
-
-    def __init__(self, jid, password, verify_security=False):
-        super().__init__(jid, password, verify_security)
-        print(self.__class__.__name__)
 
     @abstractmethod
     def tolog(self):
