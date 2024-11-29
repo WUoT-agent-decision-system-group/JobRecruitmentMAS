bac19cb466bf3496b757c680ff912fb1aaa74d1a
nowakmikolaj <nowak.mikolaj19@gmail.com>
2024-11-29 21:56:50 +0100

    Fix logger - one per agent instance

--- app/utils/log_config.py
+++ app/utils/log_config.py
@@ -1,21 +1,42 @@
-import json
 import logging
 import logging.config
+import sys
 from pathlib import Path
-
-CONFIG_FILE = "app/logging-config.json"
 
 
 class LogConfig:
     @staticmethod
-    def load_config(name: str):
+    def get_logger(name: str) -> logging.Logger:
         """Loads logger config"""
-
-        with open(CONFIG_FILE, 'r', encoding='utf-8') as cfg:
-            config = json.load(cfg)
 
         log_file = Path("/app/logs") / f"{name}.log"
         log_file.parent.mkdir(parents=True, exist_ok=True)
-        config["handlers"]["file"]["filename"] = str(log_file)
 
-        logging.config.dictConfig(config)
+        logger = logging.getLogger(name)
+
+        file_handler = LogConfig.__get_file_handler(log_file)
+        console_handler = LogConfig.__get_console_handler()
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+        logger.setLevel(logging.DEBUG)
+
+        return logger
+
+    @staticmethod
+    def __get_file_handler(log_file: Path) -> logging.FileHandler:
+        file_handler = logging.FileHandler(log_file)
+        # file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(LogConfig.__get_formatter())
+        return file_handler
+
+    @staticmethod
+    def __get_console_handler() -> logging.FileHandler:
+        file_handler = logging.StreamHandler(sys.stdout)
+        # file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(LogConfig.__get_formatter())
+        return file_handler
+
+    @staticmethod
+    def __get_formatter() -> logging.Formatter:
+        return logging.Formatter(
+            "%(asctime)s.%(msecs)03d [%(name)s] %(levelname)s: %(message)s")
