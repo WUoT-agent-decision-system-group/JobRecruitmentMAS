ea751e7b78416a52cbad12847064d761fc5ab9ea
Jakub Urbański <kuba.urban01@gmail.com>
2024-12-09 17:09:08 +0100

    Added TriggerAnalysis protocol

--- app/agents/ApplicationAnalyzerAgent.py
+++ app/agents/ApplicationAnalyzerAgent.py
@@ -1,16 +1,21 @@
 import spade.behaviour
 
-from app.dataaccess.model.RecruitmentStage import RecruitmentStage
-from app.modules.RecruitmentStageModule import RecruitmentStageModule
+from app.dataaccess.model.JobOffer import ApplicationStatus
+from app.modules.JobOfferModule import JobOfferModule
 
 from .base.BaseAgent import BaseAgent
 
 
 class ApplicationAnalyzerAgent(BaseAgent):
+    _instances: int = 0
+
     def __init__(
         self,
     ):
-        super().__init__("analyzer")
+        ApplicationAnalyzerAgent._instances = ApplicationAnalyzerAgent._instances + 1
+        super().__init__(f"{ApplicationAnalyzerAgent._instances}")
+
+        self.jobOfferModule = JobOfferModule(self.agent_config.dbname, self.logger)
 
         # behaviours
         self.analyzeBehav: Analyze = None
@@ -36,6 +41,15 @@ class Analyze(spade.behaviour.CyclicBehaviour):
         msg = await self.receive(timeout=10) # TODO: is timeout needed?
         if msg:
             self.agent.logger.info("Received message: %s", msg.body)
+
+            body = msg.body.split("%")
+
+            # ProcessApplication activity
+            result = self.agent.jobOfferModule.change_application_status(
+                body[0],
+                [body[1]],
+                ApplicationStatus.IN_ANALYSIS,
+            )
         else:
             self.agent.logger.info(
                 "No message received within the timeout."
