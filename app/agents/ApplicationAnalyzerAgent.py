81dd0e882b833c1bfa305e748d34b05a4ded8f5c
Jakub Urbański <kuba.urban01@gmail.com>
2024-12-09 17:22:07 +0100

    Added analysis response sending

--- app/agents/ApplicationAnalyzerAgent.py
+++ app/agents/ApplicationAnalyzerAgent.py
@@ -1,7 +1,10 @@
+import random
 import spade.behaviour
 
+from app.agents.RecruitmentManagerAgent import RecruitmentManagerAgent
 from app.dataaccess.model.JobOffer import ApplicationStatus
 from app.modules.JobOfferModule import JobOfferModule
+from app.utils.configuration import MASConfiguration
 
 from .base.BaseAgent import BaseAgent
 
@@ -14,6 +17,8 @@ class ApplicationAnalyzerAgent(BaseAgent):
     ):
         ApplicationAnalyzerAgent._instances = ApplicationAnalyzerAgent._instances + 1
         super().__init__(f"{ApplicationAnalyzerAgent._instances}")
+
+        self.rmentJID = self.config.agents[RecruitmentManagerAgent.__class__.__name__].jid
 
         self.jobOfferModule = JobOfferModule(self.agent_config.dbname, self.logger)
 
@@ -38,18 +43,42 @@ class Analyze(spade.behaviour.CyclicBehaviour):
     async def run(self):
         self.agent.logger.info("Analyze behaviour run.")
 
+        # AnalyzeRequest protocol
         msg = await self.receive(timeout=10) # TODO: is timeout needed?
         if msg:
-            self.agent.logger.info("Received message: %s", msg.body)
+            self.agent.logger.info("Received analyze request: %s", msg.body)
 
             body = msg.body.split("%")
 
-            # ProcessApplication activity
             result = self.agent.jobOfferModule.change_application_status(
                 body[0],
                 [body[1]],
                 ApplicationStatus.IN_ANALYSIS,
             )
+
+            if result:
+                # RateCandidate activity
+                analysis_result = random.randint(0, 100)
+
+                result = self.agent.jobOfferModule.change_application_status(
+                    body[0],
+                    [body[1]],
+                    ApplicationStatus.ANALYZED,
+                )
+
+                if result:
+                    # AnalyzeResponse protocol
+                    msg = await self.agent.prepare_message(
+                        f"{self.rmentJID}_{body[0]}_{body[1]}@{self.config.server.name}", 
+                        ["response"], 
+                        ["analyze"], 
+                        f"{analysis_result}"
+                    )
+
+                    await self.send(msg)
+            
+                    self.agent.logger.info("Sent message to rm agent with the CV analysis result.")
+
         else:
             self.agent.logger.info(
                 "No message received within the timeout."
